9cc8b630db56c0b75b2830b821e6180e19e24f41
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-04-02 20:40:08 -0400

    finished skills

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,37 @@
 import React from 'react'
-import './About.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import guitar from '../../assets/guitar.JPG'
+import './About.scss'
 
 export default function About() {
-  return <div className='About'>about.js</div>
+  return (
+    <div className='About'>
+      <Row
+        style={{ width: '80vw', color: 'white' }}
+        className='align-items-center'
+      >
+        <Col lg={6}>
+          <h1>From music to code {'</>'}</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            molestie, metus et commodo ullamcorper, tortor ex eleifend est, id
+            tristique leo lectus in lacus. Sed sagittis justo eget leo
+            efficitur, et pellentesque neque aliquet. Pellentesque et erat et
+            velit tincidunt pulvinar. Fusce nec augue molestie, pharetra mi
+            eget, ullamcorper dolor. Etiam et quam risus. Proin sed elit ut nisi
+            fermentum bibendum. Ut id imperdiet erat, a fringilla nunc.
+            Suspendisse tincidunt lobortis orci, accumsan convallis ex porttitor
+            sollicitudin. Pellentesque congue mattis arcu a pellentesque. Aenean
+            facilisis dui sit amet tellus lobortis auctor. Pellentesque
+            dignissim nisi sed mi cursus dictum vel.
+          </p>
+        </Col>
+        <Col lg={6}>
+          <Image src={guitar} className='about-img' fluid />
+        </Col>
+      </Row>
+    </div>
+  )
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,105 @@
 import React from 'react'
-import './Skills.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import './Skills.scss'
+import node from '../../assets/node.png'
+import java from '../../assets/java.png'
+import mssql from '../../assets/mssql.png'
+import postgres from '../../assets/postgres.png'
+import js from '../../assets/js.png'
+import html from '../../assets/html.png'
+import css from '../../assets/css.png'
+import scss from '../../assets/scss.png'
+import express from '../../assets/express.png'
+import bootstrap from '../../assets/bootstrap.png'
+import react from '../../assets/react.png'
+import jquery from '../../assets/jquery.gif'
 
 export default function Skills() {
-  return <div className='Skills'>Skills.js</div>
+  return (
+    <div className='Skills'>
+      <h1 className='skills-heading pb-4'>Skills</h1>
+      <Row style={{ overflowX: 'auto' }}>
+        <Col md={4}>
+          <Row>
+            <Col md={6}>
+              <Row>
+                <Col>
+                  <Image src={node} className='skills-img mb-4' />
+                </Col>
+                <Col>
+                  <Image src={java} className='skills-img mb-4' />
+                </Col>
+              </Row>
+            </Col>
+            <Col md={6}>
+              <Row>
+                <Col>
+                  <Image src={postgres} className='skills-img mb-4' />
+                </Col>
+                <Col>
+                  <Image src={mssql} className='skills-img mb-4' />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <h2 className='mt-2'>Backend</h2>
+          <hr className='hr-color mb-5' />
+        </Col>
+        <Col md={4}>
+          <Row>
+            <Col md={6}>
+              <Row>
+                <Col>
+                  <Image src={js} className='skills-img mb-4' />
+                </Col>
+                <Col>
+                  <Image src={html} className='skills-img mb-4' />
+                </Col>
+              </Row>
+            </Col>
+            <Col md={6}>
+              <Row>
+                <Col>
+                  <Image src={css} className='skills-img mb-4' />
+                </Col>
+                <Col>
+                  <Image src={scss} className='skills-img mb-4' />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <h2 className='mt-2'>Frontend</h2>
+          <hr className='hr-color mb-5' />
+        </Col>
+        <Col md={4}>
+          <Row>
+            <Col md={6}>
+              <Row>
+                <Col>
+                  <Image src={express} className='skills-img mb-4' />
+                </Col>
+                <Col>
+                  <Image src={react} className='skills-img react-logo mb-4' />
+                </Col>
+              </Row>
+            </Col>
+            <Col md={6}>
+              <Row>
+                <Col>
+                  <Image src={jquery} className='skills-img mb-4' />
+                </Col>
+                <Col>
+                  <Image src={bootstrap} className='skills-img mb-4' />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <h2 className='mt-2'>Frameworks</h2>
+          <hr className='hr-color' />
+        </Col>
+      </Row>
+    </div>
+  )
 }
