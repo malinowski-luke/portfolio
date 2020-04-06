4cff6bd5bf8ec8893e9d4457f7a87a164c075bb9
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-04-05 20:09:55 -0400

    fixed bootstrap issue and testing mobile full screen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from './components/About/About'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
 
 function App() {
