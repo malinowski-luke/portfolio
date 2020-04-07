170afbd3f210767dd4db24ee7b861a25059729c0
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-04-07 12:32:04 -0400

    fixed overflowing text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ export default function About() {
         style={{ width: '80vw', color: 'white' }}
         className='align-items-center'
       >
-        <Col lg={6}>
+        <Col lg={6} style={{ padding: '0px' }}>
           <h1 className='about-header'>From music to code {'</>'}</h1>
           <p className='about-text mb-3 mb-md-0'>
             Audio Engineer turned Fullstack Web Developer, I found my passion
@@ -20,17 +20,25 @@ export default function About() {
             track of Audio Engineering - a musician at heart, I practiced
             producing and mixing audio for a variety of uses. Along the way, I
             found interest in personal projects that lead to the pivot of my
-            career path. I started with Javascript, jQuery and
-            Bootstrap. After the completion of personal apps and calculators, I
-            moved onto a personal project called Jeep Things - a culmination of
-            my skills, Jeep Things
-            solved a problem that was close to home - a site for car lovers and
-            rebuilders to trade, sell, and buy auto parts - something that my
-            family often did.<br/>
-            As my interest grew along with my ambition to further my career in web development, I applied to DevMountain a coding  boot camp in Lehi, Utah. There, I studied full stack development using the following technologies Postgres, Node, React, Express and Redux. After a fulfilling 3 months, I recently graduated with the honor of winning the best technology award for my personal project, ready to enter the field of web development with a new set of skills and knowledge I hadn't had before. I am currently in New York seeking my next endeavor in web programming. 
+            career path. I started with Javascript, jQuery and Bootstrap. After
+            the completion of personal apps and calculators, I moved onto a
+            personal project called Jeep Things - a culmination of my skills,
+            Jeep Things solved a problem that was close to home - a site for car
+            lovers and rebuilders to trade, sell, and buy auto parts - something
+            that my family often did.
+            <br />
+            As my interest grew along with my ambition to further my career in
+            web development, I applied to DevMountain a coding boot camp in
+            Lehi, Utah. There, I studied full stack development using the
+            following technologies Postgres, Node, React, Express and Redux.
+            After a fulfilling 3 months, I recently graduated with the honor of
+            winning the best technology award for my personal project, ready to
+            enter the field of web development with a new set of skills and
+            knowledge I hadn't had before. I am currently in New York seeking my
+            next endeavor in web programming.
           </p>
         </Col>
-        <Col lg={6}>
+        <Col lg={6} style={{ padding: '0px' }}>
           <Image src={guitar} className='about-img' fluid />
         </Col>
       </Row>
