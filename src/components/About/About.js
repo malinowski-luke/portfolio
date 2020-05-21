8733055ff6a57ea4ce68458dd00556c77cff1f0d
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-05-21 10:52:21 -0400

    test

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Row, Col, Image } from 'react-bootstrap'
-import guitar from '../../assets/icons/guitar.JPG'
+import guitar from '../../assets/guitar.jpg'
 import './About.scss'
 
 export default function About() {
