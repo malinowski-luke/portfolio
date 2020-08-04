c67508fd53c57e9bc6df024adb56fa1e8f5a77b9
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MBP.fios-router.home>
2020-08-04 19:14:06 -0400

    typo fix in home code animation

--- src/utils/homeStrCode.js
+++ src/utils/homeStrCode.js
@@ -1,1 +1,1 @@
-export default 'const wakeUp = () => {\n\tif ( tired() ) {\n\t\tdrinkCoffee();\n\t} else {\n\t\tkeepCoding();\n\t}\n});\n\nwindow.onload = wakeup; '
+export default 'const wakeUp = () => {\n\tif ( tired() ) {\n\t\tdrinkCoffee();\n\t} else {\n\t\tkeepCoding();\n\t}\n});\n\nwindow.onload = wakeUp; '
