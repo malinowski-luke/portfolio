c0080482cec8ece873df1144a6b69700a1f5a23c
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MBP.fios-router.home>
2020-08-08 15:11:45 -0400

    typos fixed in projects text

--- src/utils/projectsArr.js
+++ src/utils/projectsArr.js
@@ -17,14 +17,14 @@ export default [
     link: 'http://www.jeepthingsapp.com/#/',
     github: 'https://github.com/malinowskil123/jeepthings',
     title: 'JeepThings',
-    text: `A marketplace for jeep lovers and rebuilders to trade, sell, and buy auto parts. Users can filter parts by keywords and sort them by price. This app also utilizes the google maps api to show users where a part is located on a map. Users also receive welcome and update emails as well as emails from other users regarding the parts they posted.`,
+    text: `A marketplace for jeep lovers and rebuilders to trade, sell, and buy auto parts. Users can filter parts by keywords and sort them by price. This app utilizes the google maps api to show users where a part is located on a map. Users also receive welcome and update emails as well as emails from other users regarding the parts they posted.`,
   },
   {
     image: notepad,
     coverImage: notepadCover,
     github: 'https://github.com/malinowskil123/java-swing-notepad',
     title: 'NotePad',
-    text: `A fun pet project that aimed to build a Java text editor similar to a notepad application. User can create, overwrite and save .txt files on their computer. It includes a fully functioning edit section with copy, paste, cut, undo and redo functions. This app also tracks your word count and sentence count along with a few other metrics and feeds into a alogrithm which outputs a Flesch text complexity score`,
+    text: `A fun pet project that aimed to build a Java text editor similar to a desktop notepad application. User can create, overwrite and save .txt files on their computer. It includes a fully functioning edit section with copy, paste, cut, undo and redo functions. This app also tracks your word count and sentence count along with a few other metrics and feeds into a alogrithm which outputs a Flesch text complexity score.`,
   },
   {
     image: calculator,
@@ -32,7 +32,7 @@ export default [
     link: 'https://calculator-js-lm.herokuapp.com/',
     github: 'https://github.com/malinowskil123/calculator',
     title: 'Calculator',
-    text: `Instead of using your smart-phones calculator, now there's a web alternative. Takes in simple arithmetic and alos checks your input for syntax errors. This app was developed using JavaScript, jQuery, Html, and Css`,
+    text: `Instead of using your smart-phones calculator, now there's a web alternative. Takes in a simple arithmetic and also checks your input for syntax errors. This app was developed using JavaScript, jQuery, Html, and Css.`,
   },
   {
     image: junkedup,
@@ -48,6 +48,6 @@ export default [
     coverImage: heloCover,
     github: 'https://github.com/malinowskil123/helo',
     title: 'Helo',
-    text: `Helo is a social media app, which allows users to create profiles, post blogs, and interact with other users posts. Users can also filter posts by a keyword and post id's to find their own posts`,
+    text: `Helo is a social media app, which allows users to create profiles, post blogs, and interact with other users posts. Users can also filter posts by a keyword and post id's to find their own posts.`,
   },
 ]
