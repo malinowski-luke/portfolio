7d421f40b24fd646fc81634d4ebf61aee3b772e3
Lukasz Malinowski <lukaszmalinowski@MacBook-Pro.local>
2020-09-20 22:25:01 -0400

    min- height vh  bug fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useWindowHeight } from '@react-hook/window-size'
 import Nav from './components/Nav/Nav'
 import routes from './routes'
 import './App.scss'
 
 function App() {
+  const clientHeight = useWindowHeight()
+
+  useEffect(() => {
+    console.log(clientHeight)
+    document.getElementById('app').style.minHeight = `${clientHeight}px`
+  }, [clientHeight])
+
   return (
-    <div className='App text-center'>
+    <div id='app' className='App text-center'>
       <Nav />
       <div className='container'>{routes}</div>
     </div>
