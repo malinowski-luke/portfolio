86a94848feff2f74e3c79ce1e0492927d3647cc5
Luke Malinowski <malinowski.luke123@gmail.com>
2020-11-07 22:41:50 -0500

    updated resume link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -27,7 +27,7 @@ export default function Contact() {
         <h5>linkedin</h5>
       </a>
       <a
-        href='https://drive.google.com/file/d/1dZujMhUkbDsQF4XOw--2oR2RVLZndDNI/view?usp=sharing'
+        href='https://docs.google.com/document/d/e/2PACX-1vRZsT9_8BUT_2ENiwVJ-FEfJlEo7b_G5U_ejDQMt6AKwJlnutHpVmlsImdyCtLKjAy9E61YpadcpDuB/pub'
         target='_blank'
         rel='noopener noreferrer'
       >
