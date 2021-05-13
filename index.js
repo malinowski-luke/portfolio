8aff27e9bedb41ea7006887290645da82c4c6429
malinowskil123 <malinowski.luke123@gmail.com>
2021-05-12 23:28:09 -0400

    fixed issue line height

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ import path from 'path'
 
 const application = express()
 
+// middleware
 // React Build
 application.use(express.static(path.join(__dirname, 'build')))
-// if we hit not-found re-direct on the front-end then send build index.html file
-application.get('*', (request, response) =>
-  response.sendFile('index.html', {
-    root: path.join(__dirname, './build/'),
-  })
-)
-
 // Cross Origin
 application.use(
   cors({
     credentials: true,
     methods: ['GET'],
     maxAge: 84600,
+  })
+)
+
+// if we hit not-found re-direct on the front-end then send build index.html file
+application.get('*', (request, response) =>
+  response.sendFile('index.html', {
+    root: path.join(__dirname, './build/'),
   })
 )
 
@@ -36,8 +37,6 @@ const PORT = process.env.PORT || 3000
 
 application.listen(PORT, () => {
   if (process.env.NODE_ENV === 'development') {
-    console.log(
-      `===================================== \nserver listening on: ${PORT} \n=====================================`
-    )
+    console.log(`server listening on: ${PORT}`)
   }
 })
