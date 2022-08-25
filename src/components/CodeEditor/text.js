747803a01a375db0d05330e533c1c771180d5eea
Luke Malinowski <48255282+malinowskil123@users.noreply.github.com>
2022-08-24 21:29:47 -0400

    node server set up (#14)

--- src/components/CodeEditor/text.js
+++ src/components/CodeEditor/text.js
@@ -1,5 +1,7 @@
-export default `// 👋 Hi I'm Luke\n
+const TEXT = `// 👋 Hi I'm Luke\n
 // I am a full-stack software engineer based in NYC,
 // currently working @ Tropic 🌴 inovating SaaS procurment.
 
 // Feel free to reach out & say hello...\n`
+
+export default TEXT
