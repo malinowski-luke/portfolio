686a3567581051a7e55af521b8b20d9e4d6655e3
malinowskil123 <malinowskil123@users.noreply.github.com>
2022-08-25 00:10:07 +0000

    Prettified Code!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import BaseLayout from "./components/BaseLayout/BaseLayout";
-import CodeEditor from "./components/CodeEditor/CodeEditor";
-import Footer from "./components/Footer/Footer";
+import React from 'react'
+import BaseLayout from './components/BaseLayout/BaseLayout'
+import CodeEditor from './components/CodeEditor/CodeEditor'
+import Footer from './components/Footer/Footer'
 
-import "./App.scss";
+import './App.scss'
 
 const App = () => (
   <div id="app" className="App container">
@@ -12,6 +12,6 @@ const App = () => (
     </BaseLayout>
     <Footer />
   </div>
-);
+)
 
-export default App;
+export default App
