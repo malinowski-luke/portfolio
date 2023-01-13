97f61fa5ef3f5cec1e5d21374788bb69e78f70bf
Luke Malinowski <48255282+malinowskil123@users.noreply.github.com>
2023-01-13 17:28:01 -0500

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className="Footer">
       <Text>
-        <Link url="https://github.com/malinowskil123a">Github</Link> |{' '}
+        <Link url="https://github.com/malinowskil123">Github</Link> |{' '}
         <Link url="https://www.linkedin.com/in/luke-malinowski-50328/">Linkedin</Link>
       </Text>
     </div>
