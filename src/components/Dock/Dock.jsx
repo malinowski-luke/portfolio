a19e5ed7dfdc8300171e29928e5faa3dda2ff9b2
Luke Malinowski <48255282+malinowskil123@users.noreply.github.com>
2023-01-21 23:48:10 -0500

    add soundcloud link and reafctor links

--- src/components/Dock/Dock.jsx
+++ src/components/Dock/Dock.jsx
@@ -2,39 +2,39 @@ import React from 'react'
 
 import GithubLogo from '../../assets/github.png'
 import LinkedinLogo from '../../assets/linkedin.png'
+import SoundcloudLogo from '../../assets/soundcloud.png'
 import Trash from '../../assets/trash.png'
 
 import './Dock.scss'
+import links from './links'
 
 const Dock = () => {
   return (
     <div data-testid="dock" className="dock">
-      <a
-        data-testid="github-link"
-        href="https://github.com/malinowskil123"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="dock-item"
-      >
+      <a data-testid="github-link" href={links.github} target="_blank" rel="noopener noreferrer" className="dock-item">
         <img src={GithubLogo} alt="github link icon" />
       </a>
       <a
         data-testid="linkedin-link"
-        href="https://www.linkedin.com/in/luke-malinowski-50328/"
+        href={links.linkedin}
         target="_blank"
         rel="noopener noreferrer"
         className="dock-item"
       >
         <img src={LinkedinLogo} alt="linkedin link icon" />
       </a>
-      <div></div>
       <a
-        data-testid="trash-link"
-        href="https://www.php.net/"
+        data-testid="soundcloud-link"
+        href={links.soundcloud}
         target="_blank"
         rel="noopener noreferrer"
         className="dock-item"
       >
+        <img src={SoundcloudLogo} alt="soundcloud link icon" />
+      </a>
+      {/* vertical break */}
+      <div></div>
+      <a data-testid="trash-link" href={links.trash} target="_blank" rel="noopener noreferrer" className="dock-item">
         <img src={Trash} alt="trash link icon" />
       </a>
     </div>
