1ec5375054cdfaefd40db975ce467488a91e85e8
Luke Malinowski <48255282+malinowskil123@users.noreply.github.com>
2023-01-21 23:19:10 -0500

    install and setup vh-check (#27)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import BaseLayout from './components/BaseLayout/BaseLayout'
 import CodeEditor from './components/CodeEditor/CodeEditor'
 import Dock from './components/Dock/Dock'
