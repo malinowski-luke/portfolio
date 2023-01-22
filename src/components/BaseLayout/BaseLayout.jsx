fca164bca511d4a4aa52a91882178a29e727bbc4
Luke Malinowski <48255282+malinowskil123@users.noreply.github.com>
2023-01-21 23:14:18 -0500

    install and setup vh-check

--- src/components/BaseLayout/BaseLayout.jsx
+++ src/components/BaseLayout/BaseLayout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import vhCheck from 'vh-check'
 
 import './BaseLayout.scss'
 
 const BaseLayout = ({ children }) => {
+  // eslint-disable-next-line no-unused-vars
+  const checkVh = vhCheck()
   return <div className="base-layout">{children}</div>
 }
 
